731280c3325f8ffe69f250f81859d97bd0421f6d
dennis.castro <manuel.castro.guzman@ericsson.com>
2020-03-28 00:16:42 -0600

    diseño 2 y 3

--- disenios/lib/main.dart
+++ disenios/lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:disenios/src/pages/basico_page.dart';
+import 'package:disenios/src/pages/scroll_page.dart';
+import 'package:disenios/src/pages/botones_page.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent
+      )
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'botones',
       routes: {
         'basico'  : (BuildContext context) => BasicoPage(),
-
+        'scroll'  : (BuildContext context) => ScrollPage(),
+        'botones'  : (BuildContext context) => BotonesPage(),
       },
     );
   }
